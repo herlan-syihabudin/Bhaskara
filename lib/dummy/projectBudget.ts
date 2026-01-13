26a292c24c1f8f7b2a5b803e249e44c2e31427c3
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-13 14:27:01 +0700

    Create projectBudget.ts

---
+++ lib/dummy/projectBudget.ts
@@ -0,0 +1,19 @@
+export const projectBudget = {
+  projectName: "Proyek Gudang Cikarang",
+  nilaiKontrak: 2500000000,
+  totalPO: 1200000000,
+  biayaReal: 820000000,
+  sisaBudget: 1680000000,
+  status: "AMAN",
+
+  byCategory: [
+    { name: "Material", po: 600000000, real: 420000000 },
+    { name: "Jasa", po: 400000000, real: 310000000 },
+    { name: "Alat", po: 200000000, real: 90000000 },
+  ],
+
+  byVendor: [
+    { name: "PT Beton Jaya", po: 350000000, real: 290000000 },
+    { name: "CV Listrik Abadi", po: 220000000, real: 200000000 },
+  ],
+};
