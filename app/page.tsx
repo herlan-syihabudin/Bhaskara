fce7870502389ad6d9c7ef695f46ced85c6cc614
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-13 10:41:52 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <section style={{ padding: "80px" }}>
+      <h1>PT Bhaskara Buana Mulya</h1>
+      <p>Engineering-driven General Contractor</p>
+    </section>
+  );
+}
