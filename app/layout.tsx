53e498fea651e9c44dffc29fa68762324327c546
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-13 10:41:27 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import Header from "@/components/site/Header";
+import Footer from "@/components/site/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
