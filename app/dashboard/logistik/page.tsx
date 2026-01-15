18d7f9c6ef1c94fe02d9a055880a20804148c5b9
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-15 10:56:42 +0700

    Create page.tsx

---
+++ app/dashboard/logistik/page.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/logistik/page.tsx
+export default function LogistikDashboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Logistik</h1>
+      <p className="text-sm text-gray-500 mt-2">
+        Barang masuk, keluar, dan distribusi
+      </p>
+    </div>
+  );
+}
