487ffe87ba8425fcf7d6f1198257fd47d11edcb8
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-15 10:55:05 +0700

    Create page.tsx

---
+++ app/dashboard/pm/page.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/pm/page.tsx
+export default function PMDashboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Project Manager</h1>
+      <p className="text-sm text-gray-500 mt-2">
+        Monitoring progres & realisasi proyek
+      </p>
+    </div>
+  );
+}
