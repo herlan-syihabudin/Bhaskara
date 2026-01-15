fb9a59abe228c79c3be70f205dcc70e0e0630d88
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-15 10:54:33 +0700

    Create page.tsx

---
+++ app/dashboard/owner/page.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/owner/page.tsx
+export default function OwnerDashboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Owner Dashboard</h1>
+      <p className="text-sm text-gray-500 mt-2">
+        Ringkasan seluruh proyek & keuangan perusahaan
+      </p>
+    </div>
+  );
+}
