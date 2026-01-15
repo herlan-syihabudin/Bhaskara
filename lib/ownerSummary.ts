9f847a6b3c662d1a7aafb5af0ee26161e906468c
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-15 11:46:55 +0700

    Update ownerSummary.ts

--- lib/ownerSummary.ts
+++ lib/ownerSummary.ts
@@ -1,28 +1,15 @@
 import { projects } from "@/lib/dummy/projectBudget";
-import { statusFromBudget } from "@/lib/budgetEngine";
 
 export function getOwnerSummary() {
   const totalProyek = projects.length;
-
-  const totalKontrak = projects.reduce(
-    (a, b) => a + b.nilaiKontrak,
-    0
-  );
-
-  const totalBiaya = projects.reduce(
-    (a, b) => a + b.biayaReal,
-    0
-  );
-
-  const totalSisa = totalKontrak - totalBiaya;
-
-  const status = statusFromBudget(totalKontrak, totalBiaya);
+  const totalKontrak = projects.reduce((a, b) => a + b.nilaiKontrak, 0);
+  const totalBiaya = projects.reduce((a, b) => a + b.biayaReal, 0);
 
   return {
     totalProyek,
     totalKontrak,
     totalBiaya,
-    totalSisa,
-    status,
+    totalSisa: totalKontrak - totalBiaya,
+    status: totalBiaya / totalKontrak > 0.9 ? "BAHAYA" : "AMAN",
   };
 }
