5311900a67bc8895d6870cd860b22785606785c4
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-16 16:04:01 +0700

    Delete lib/engine/realCost.ts

--- lib/engine/realCost.ts
+++
@@ -1,8 +0,0 @@
-// lib/engine/realCost.ts
-import { realCosts } from "@/lib/data/realCosts";
-
-export function getRealCostByProject(projectId: string) {
-  return realCosts
-    .filter((c) => c.projectId === projectId)
-    .reduce((acc, c) => acc + c.amount, 0);
-}
