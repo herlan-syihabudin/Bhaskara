98d84b28244d3a9f3e45eea63c9d4612883d0d76
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-16 15:51:00 +0700

    Update projects.ts

--- lib/data/projects.ts
+++ lib/data/projects.ts
@@ -1,9 +1,14 @@
 // lib/data/projects.ts
+
 export type ProjectStatus = "ONGOING" | "DONE";
 
 export type Project = {
   id: string;
   name: string;
   nilaiKontrak: number;
+
+  // ⬇️ WAJIB ADA
+  biayaReal: number;
+
   status: ProjectStatus;
 };
