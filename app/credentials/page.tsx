77d803dd578719a677fb6c05c2c937e583b4904f
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-17 20:08:12 +0700

    Create page.tsx

---
+++ app/credentials/page.tsx
@@ -0,0 +1,11 @@
+import ClientList from "@/components/credentials/ClientList";
+import CallToAction from "@/components/home/CallToAction";
+
+export default function CredentialsPage() {
+  return (
+    <>
+      <ClientList />
+      <CallToAction />
+    </>
+  );
+}
