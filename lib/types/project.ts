c53fb620c8e7c81ee8a74df24dd9e993fa93a69c
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-19 12:53:38 +0700

    Update project.ts

--- lib/types/project.ts
+++ lib/types/project.ts
@@ -1,8 +1,10 @@
+export type BudgetStatus = "AMAN" | "WARNING" | "BAHAYA";
+
 export type ProjectSummary = {
   project_id: string;
   project_name: string;
   nilaiKontrak: number;
   biayaReal: number;
   sisaBudget: number;
-  statusBudget: "AMAN" | "WARNING" | "BAHAYA";
+  statusBudget: BudgetStatus;
 };
